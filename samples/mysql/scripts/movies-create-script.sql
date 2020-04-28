2200f62256acd2b978ae0b9473ade5b383f9cf27
Guido Mantilla <guido.mantilla@yahoo.com>
2020-04-28 07:45:03 -0500

    Fixes Commit

--- samples/mysql/scripts/movies-create-script.sql
+++ samples/mysql/scripts/movies-create-script.sql
@@ -1,5 +1,5 @@
 drop database if exists movies;
-create database movies;
+create database movies /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 use movies;
 
 create table movie(
--- samples/mysql/scripts/toolbox-create-script.sql
+++ samples/mysql/scripts/toolbox-create-script.sql
@@ -16,7 +16,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 drop database if exists toolbox;
-create database toolbox;
+create database toolbox /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 use toolbox;
 
 --
